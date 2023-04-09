eaa526b4706f3beb139e0923ae588de9c04a599f
Andy Phan <andyphan@live.com.au>
2023-04-09 18:05:50 +0930

    Styled output section

--- calculator.js
+++ calculator.js
@@ -2,6 +2,8 @@ const people = document.querySelector('#people');
 const peopleError = document.querySelector('.people-error');
 const tipItems = document.querySelectorAll('.input-tip-item:not(.input-tip-item-custom)');
 
+// Validate Number of people input
+// "0" invokes error styling
 function validatePeople() {
     if(people.value === "0") {
         people.classList.add('error');
@@ -12,6 +14,7 @@ function validatePeople() {
     }
 }
 
+// Sets data attribute (data-tip) of selected tip item and removes for all others
 function setSelectedTip(selected) {
     tipItems.forEach(tip => {
         if(selected === tip) {
@@ -22,10 +25,12 @@ function setSelectedTip(selected) {
     });
 }
 
+// Add event for each Tip "button"
 tipItems.forEach(tip => {
     tip.addEventListener('click', () => {
         setSelectedTip(tip);
     });
 });
 
+// Event for people input
 people.addEventListener('keyup', validatePeople);
