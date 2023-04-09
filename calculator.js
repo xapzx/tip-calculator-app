edc827a6b9de3059daacbfac8c99869462317179
Andy Phan <andyphan@live.com.au>
2023-04-09 18:40:00 +0930

    Implemented functionality to disable and enable button according to filled input fields

--- calculator.js
+++ calculator.js
@@ -1,6 +1,9 @@
 const people = document.querySelector('#people');
 const peopleError = document.querySelector('.people-error');
 const tipItems = document.querySelectorAll('.input-tip-item:not(.input-tip-item-custom)');
+const inputs = document.querySelectorAll('input:not(.input-tip-item-custom)');
+const customTip = document.querySelector('.input-tip-item-custom');
+const resetBtn = document.querySelector('.reset');
 
 // Validate Number of people input
 // "0" invokes error styling
@@ -25,12 +28,56 @@ function setSelectedTip(selected) {
     });
 }
 
+// Check if all required input fields are filled
+function checkAllFilled() {
+    let filled = true;
+
+    inputs.forEach(input => {
+        if(input.value === "") {
+            filled = false;
+        }
+    })
+    return filled;
+}
+
+// Check if tip has been selected/inputted
+function checkSelectedTip() {
+    let selected = false;
+    tipItems.forEach(item => {
+        if(item.getAttribute('data-tip') === "true") {
+            selected = true;
+        }
+    })
+    
+    if(!selected && customTip.value === "") {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+// Check if all input fields are filled
+// Activate reset button if all filled
+function setButtonStatus() {
+    if(checkSelectedTip() && checkAllFilled()) {
+        resetBtn.setAttribute('data-disabled', 'false');
+    } else {
+        resetBtn.setAttribute('data-disabled', 'true');
+    }
+}
+
 // Add event for each Tip "button"
 tipItems.forEach(tip => {
     tip.addEventListener('click', () => {
         setSelectedTip(tip);
+        setButtonStatus();
     });
 });
 
 // Event for people input
 people.addEventListener('keyup', validatePeople);
+
+// Check fields with each input
+inputs.forEach(input => {
+    input.addEventListener('keyup', setButtonStatus);
+});
